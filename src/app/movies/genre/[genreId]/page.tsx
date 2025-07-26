ac683bfcfa6b067fdf5d524b3a40d2293469b34e
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-07-26 13:42:19 +0700

    fix:same as before :( 1

--- src/app/movies/genre/[genreId]/page.tsx
+++ src/app/movies/genre/[genreId]/page.tsx
@@ -1,17 +1,15 @@
 import GridMovieList from "@/components/common/GridMovieList"
 
-type PageProps = {
-  params: {
-    genreId: string;
-  };
-}
 
-const MoviesByGenrePage  = async ({ params : { genreId }} : PageProps) => {
+
+const MoviesByGenrePage  = async (props : { params : Promise<{ genreId : string }> }) => {
+
+  const params = await props.params
 
   return (
     <div>
-      <h1 className="text-center font-bold text-xl mb-10">Genre Id {genreId}</h1>
-      <GridMovieList endpoint={`/discover/movie?with_genres=${genreId}`}/>
+      <h1 className="text-center font-bold text-xl mb-10">Genre Id {params.genreId}</h1>
+      <GridMovieList endpoint={`/discover/movie?with_genres=${params.genreId}`}/>
     </div>
   )
 }
