30c5606acf8c87e97f8dac335915b6b5f2336627
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-07-29 16:35:31 +0700

    ui/create skeleton components for detail movie page

--- src/components/detailMoviePage/PreviewVideo.tsx
+++ src/components/detailMoviePage/PreviewVideo.tsx
@@ -6,9 +6,9 @@ const PreviewVideo = ({ preview_video_key } : { preview_video_key : string }) =>
     const [showVideo, setShowVideo] = useState(true)
 
   return (
-    <div className={`fixed z-10 right-2 bottom-1 text-end aspect-[16/9] lg:w-96`}>
+    <div className={`fixed z-10 right-2 bottom-1 text-end lg:w-96`}>
         <button onClick={() => setShowVideo(p => !p)} title="Hide Preview video" className=" bg-white/70 hover:bg-white/90 active:bg-white cursor-pointer text-black">{showVideo ? <X /> : <span className="p-1">Show Video</span>}</button>
-        {showVideo && <iframe src={`https://www.youtube.com/embed/${preview_video_key}`} className="size-full rounded"></iframe>}
+        {showVideo && <iframe src={`https://www.youtube.com/embed/${preview_video_key}`} className="size-full rounded aspect-[16/9]"></iframe>}
     </div>
   )
 }
