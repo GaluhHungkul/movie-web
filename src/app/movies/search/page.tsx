a63242bb0f2d5e4959da7f54398387c382fde3f8
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-08-01 14:40:20 +0700

    fix/params promise type

--- src/app/movies/search/page.tsx
+++ src/app/movies/search/page.tsx
@@ -2,17 +2,17 @@ import GridMovieList from "@/components/common/GridMovieList"
 import { FC } from "react"
 
 type Props = {
-    searchParams : {
-        query : string
-    }
+  searchParams : Promise<{ query : string }>
 }
 
-const SearchMoviePage : FC<Props> = ({ searchParams : { query } }) => {
-    console.log({query})
+const SearchMoviePage : FC<Props> = async (props) => {
+
+  const searchParams = await props.searchParams
+
   return (
     <div>
-        <h1 className="my-5 text-sm md:text-base lg:text-xl">Results for {'"'}<span className="text-white/70">{query}</span>{'"'}</h1>
-        <GridMovieList endpoint={`/search/movie?query=${query}`}/>
+        <h1 className="my-5 text-sm md:text-base lg:text-xl">Results for {'"'}<span className="text-white/70">{searchParams.query}</span>{'"'}</h1>
+        <GridMovieList endpoint={`/search/movie?query=${searchParams.query}`}/>
     </div>
   )
 }
