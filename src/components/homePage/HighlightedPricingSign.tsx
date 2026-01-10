57b63d449087cda18d262adfbd6cc177a8a9ee18
GaluhHungkul <muhamadgaluh31@gmail.com>
2026-01-10 22:53:07 +0700

    ui/create highlightedpricingsign component

---
+++ src/components/homePage/HighlightedPricingSign.tsx
@@ -0,0 +1,9 @@
+const HighlightedPricingSign = () => {
+  return (
+    <div className="absolute w-10 h-40 bg-red-500 shadow-2xl shadow-black right-4 -top-12 -rotate-45 flex justify-center items-center">
+        <h1 className="text-center text-white rotate-90">Most Popular</h1>
+    </div>
+  )
+}
+
+export default HighlightedPricingSign
--- src/components/homePage/PricingPlans.tsx
+++ src/components/homePage/PricingPlans.tsx
@@ -1,5 +1,6 @@
 import { pricingPlans } from '@/lib/data/pricingPlans'
 import { CheckCheck } from 'lucide-react'
+import HighlightedPricingSign from './HighlightedPricingSign'
 
 const PricingPlans = () => {
   return (
@@ -7,7 +8,8 @@ const PricingPlans = () => {
         <h1 className="text-xl md:text-2xl">More Reasons to Join</h1>
         <section className='flex flex-col items-center gap-4 mt-10 md:flex-row md:items-stretch lg:gap-10 lg:justify-center md:mt-16'>
             {pricingPlans.map(item => (
-                <div key={item.id} className={`min-w-72 rounded-xl px-6 py-8 bg-gradient-to-br from-blue-500/40 via-purple-900/40 to-red-900/40 text-white/80 text-sm duration-300 lg:hover:scale-110 md:flex-1 relative md:pb-16 hover:scale-105 hover:bg-white/10 md:min-w-0 lg:min-h-[500px] lg:max-w-80 ${item.highlighted ? "md:-translate-y-8 lg:-translate-y-12" : ""} lg:hover:scale-[103%]`}>
+                <div key={item.id} className={`min-w-72 rounded-xl px-6 py-8 bg-gradient-to-br from-blue-500/40 via-purple-900/40 to-red-900/40 text-white/80 text-sm duration-300 lg:hover:scale-110 md:flex-1 relative md:pb-16 hover:scale-105 hover:bg-white/10 md:min-w-0 lg:min-h-[500px] lg:max-w-80 ${item.highlighted ? "md:-translate-y-8 lg:-translate-y-12" : ""} lg:hover:scale-[103%] relative overflow-hidden`}>
+                    {item.highlighted && <HighlightedPricingSign />}
                     <h1 className="text-center rounded-full font-bold text-xl md:text-2xl">{item.title}</h1>
                     <h2 className="mt-2 text-center mb-8 md:text-lg">{item.price} / {item.period}</h2>
                     <ul className="space-y-2 mt-4 mb-16 lg:text-lg">
