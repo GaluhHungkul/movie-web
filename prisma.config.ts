9048b3ee64fc582df55e14aaab1a29dbee8c4f79
GaluhHungkul <muhamadgaluh31@gmail.com>
2026-01-11 22:29:00 +0700

    fix/prisma error

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,16 @@
 import 'dotenv/config'
-import type { PrismaConfig } from "prisma";
-import { env } from "prisma/config";
+import { env } from "prisma/config"
+import type { PrismaConfig } from "prisma/config"
 
 export default {
   schema: "prisma/schema.prisma",
+
   migrations: {
     path: "prisma/migrations",
-    seed: 'tsx prisma/seed.ts',
+    seed: "tsx prisma/seed.ts",
   },
-  datasource: { 
-    url: env("DATABASE_URL") 
-  }
-} satisfies PrismaConfig;
+
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+} satisfies PrismaConfig
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from "@prisma/adapter-pg"
+import { PrismaClient } from "@prisma/client"
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL
+})
 
-export const prisma =  globalForPrisma.prisma ?? new PrismaClient();
-
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient({
+    adapter
+  })
 }
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global
+
+export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
