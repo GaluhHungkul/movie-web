ffd2c45282dcd21e0721e928e97c4dab98019554
GaluhHungkul <muhamadgaluh31@gmail.com>
2026-01-17 09:13:28 +0700

    ui/scale loading ui state

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,14 +2,16 @@ import { Spinner } from "@/components/ui/spinner";
 
 export default function Loading() {
   return (
-    <div className="
-      fixed inset-0 z-50
-      bg-white/10
-      backdrop-blur-sm
-      flex items-center justify-center
-      min-h-screen
-    ">
-      <Spinner />
+    <div className="h-screen">
+      <div className="
+        backdrop-blur-sm 
+        fixed w-full left-0 top-0
+        bg-white/10
+        flex items-center justify-center
+        h-screen
+      ">
+        <Spinner className="size-20"/>
+      </div>
     </div>
   )
 }
