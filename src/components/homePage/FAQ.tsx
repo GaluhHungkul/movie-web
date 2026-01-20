4711e270a436ee0465d38974083441682523ae1f
GaluhHungkul <muhamadgaluh31@gmail.com>
2026-01-20 10:19:19 +0700

    ui/update faq component

--- src/components/homePage/FAQ.tsx
+++ src/components/homePage/FAQ.tsx
@@ -1,42 +1,50 @@
 "use client"
 
-import { faqList } from "@/lib/data/faqList"
-import { ChevronRight } from "lucide-react";
-import { useState } from "react"
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import Heading from "../common/Heading";
+import MutedText from "../common/MutedText";
+import { faqListItems } from "@/lib/data/categories";
 
-type Faq = {
-    id: number;
-    question: string;
-    answer: string; 
-}
 
 const FAQ = () => {
 
-    const [showFaq, setShowFaq] = useState<Faq | null>(null)
-
-    const handleToggleShowFaq = (value:Faq) => {
-        setShowFaq(p => {
-            if(!p) return value
-            return p.id === value.id ? null : value
-        })
-    }
 
   return (
-    <div className="w-full my-20 ">
-        <h1 className="text-xl md:text-2xl">Frequently Asked Questions</h1>
-        <section className="mt-5 space-y-2">
-            {faqList.map(item => (
-                <div key={item.id}  className={`bg-white/10  cursor-pointer ${item.id === showFaq?.id ? "bg-white/20" : ""}`}>
-                    <section onClick={() => handleToggleShowFaq(item)} className="flex justify-between hover:bg-white/15 items-center p-4">
-                        <h1 className="md:text-xl">{item.question}</h1>
-                        <ChevronRight className={`${showFaq?.id === item.id ? "rotate-90" : ""} md:scale-125`}/>
-                    </section>
-                    {item.id === showFaq?.id && <p className="px-4 py-6 border-t-2 border-black md:text-lg md:font-medium">
-                        {item.answer}
-                    </p>}
-                </div>
+    <div className="container">
+        <Heading>Frequently Asked Questions</Heading>
+        <MutedText>Find instant answers to everything you need to know about StreamVibe-from account setup to streaming quality, billing to device compatibility Still curious? Our support team is just a click away.</MutedText>
+        <Accordion
+            type="single"
+            collapsible
+            className="w-full grid md:grid-cols-2 gap-4"
+        >
+            {faqListItems.map(item => (
+                <AccordionItem key={item.id} value={`item-${item.id}`}>
+                    <AccordionTrigger className="items-center">
+                        <div className="justify-start flex items-center gap-4">
+                            <span className="bg-black-12 shrink-0 size-12 rounded-lg text-center content-center ring ring-black-15">{item.id}</span>
+                            <h1>{item.title}</h1>
+                        </div>
+                    </AccordionTrigger>
+                    <AccordionContent className="flex flex-col gap-4 text-balance">
+                    <p>
+                        Our flagship product combines cutting-edge technology with sleek
+                        design. Built with premium materials, it offers unparalleled
+                        performance and reliability.
+                    </p>
+                    <p>
+                        Key features include advanced processing capabilities, and an
+                        intuitive user interface designed for both beginners and experts.
+                    </p>
+                    </AccordionContent>
+                </AccordionItem>
             ))}
-        </section>
+        </Accordion>
     </div>
   )
 }
