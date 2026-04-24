cdd3c5a0926d7e251131b9cd9de8d08f3ebb4005
GaluhHungkul <muhamadgaluh31@gmail.com>
2026-04-24 13:44:53 +0700

    fix/next auth session bug

--- src/proxy.ts
+++ src/proxy.ts
@@ -8,11 +8,9 @@ const protectedRoutesUnderStandardPlan = ["/showmore/top_rated"]
 export async function proxy(req: NextRequest) {
 
     const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET }) 
-    // if(!token) return NextResponse.json({ message: "Unauthorized" }, { status: 401 })
     const { pathname } = req.nextUrl
 
     const isProtected = protectedRoutesWithNoToken.some(prefix => pathname.startsWith(prefix))
-
     if(isProtected && !token) return NextResponse.redirect(new URL("/", req.url))
 
     const isUnderStandarPlan = token?.subscribePlanTitle === "Basic Plan" || !token?.isSubscribe
